8aa78002f7086672068d2e2e9aa6187dd57b512e
Kasper Hegemann <kasper.hegemann@student.ehb.be>
2021-11-09 18:36:23 +0100

    added dummy data

---
+++ dummy.js
@@ -0,0 +1,32 @@
+module.exports = {
+    1: {
+        name: 'Bert',
+        age: 21,
+        bestTrick: 'kickflip'
+    },
+    2: {
+        name: 'andy',
+        age: 21,
+        bestTrick: 'heelflip'
+    },
+    3: {
+        name: 'Hendrik',
+        age: 21,
+        bestTrick: 'nolliBigspin'
+    },
+    4: {
+        name: 'Oscar',
+        age: 21,
+        bestTrick: 'varialHeel'
+    },
+    5: {
+        name: 'Aron',
+        age: 21,
+        bestTrick: 'rickFlip'
+    },
+    6: {
+        name: 'Jasper',
+        age: 21,
+        bestTrick: 'laserFlip'
+    }
+}
