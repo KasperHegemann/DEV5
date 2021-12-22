4ccc5033722996809ec3bc21d2b384980a290f14
Kasper Hegemann <kasper.hegemann@student.ehb.be>
2021-12-22 22:11:03 +0100

    still workin on it

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,13 @@
-const {
-    application
-} = require('express');
+//const { application } = require('express');
 const express = require('express');
+const knex = require("../sqlconfig.js");
+
 
 const app = express();
 
 const HOST = 'localhost';
 const PORT = 8888;
+const SQLPORT = 3000;
 
 app.use(express.json());
 
@@ -14,6 +15,25 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Kasper API test');
 });
+
+app.get("/api/allSkaters", (req, res) => {
+    knex("skaters").then((skaters) => res.json(skaters));
+});
+
+app.post("/api/addSkater", async (req, res) => {
+    if (req.body.skater) {
+        knex("skaters")
+            .insert({
+                name: req.body.name,
+                tick: req.body.trick
+            })
+            .returning("*")
+            .then((skater) => res.json(skater));
+    }
+});
+
+
+
 
 
 module.exports = app;
